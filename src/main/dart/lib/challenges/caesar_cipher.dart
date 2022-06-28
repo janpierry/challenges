b80a9324721f8cafa4d3504b3b85641691c22167
janpierry <janpierrycoelho@gmail.com>
2022-06-28 15:09:07 -0300

    feat: Add Caesar Cipher challenge (Dart)

---
+++ src/main/dart/lib/challenges/caesar_cipher.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+class CaesarCipher {
+  String _solution(String input, int k) {
+    final encodedInput = ascii.encode(input);
+    final List<int> encodedResult = [];
+    encodedInput.forEach((element) {
+      if (element >= 65 && element <= 90) {
+        final encrypted = ((element - 65 + k) % 26) + 65;
+        encodedResult.add(encrypted);
+      } else if (element >= 97 && element <= 122) {
+        final encrypted = ((element - 97 + k) % 26) + 97;
+        encodedResult.add(encrypted);
+      } else {
+        encodedResult.add(element);
+      }
+    });
+    final result = ascii.decode(encodedResult);
+    return result;
+  }
+
+  void start() {
+    final result = _solution('Always-Look-on-the-Bright-Side-of-Life', 5);
+    print(result);
+  }
+}
--- src/main/dart/lib/main.dart
+++ src/main/dart/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:challenges/challenges/caesar_cipher.dart';
 import 'package:challenges/challenges/counting_valleys.dart';
 import 'package:challenges/challenges/elements_combination.dart';
 import 'package:challenges/challenges/palindrome_index.dart';
 
 void main(List<String> args) {
-  //ElementsCombination().start();
+  // ElementsCombination().start();
   // PalindromeIndex().start();
-  CountingValleys().start();
+  // CountingValleys().start();
+  CaesarCipher().start();
 }
